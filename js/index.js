f28f370596f12656297df24cc53de7aa82f11222
luciabonsack <luciabonsack@gmail.com>
2024-06-22 21:20:38 -0700

    Updates from user feedback iterations

--- js/index.js
+++ js/index.js
@@ -19,20 +19,46 @@ $("p").on({
     },
   });
 
+  $("#cs2").on({
+    mouseenter: function(){
+        $("#cs1").animate({height: "120%"});
+    },
+    mouseleave: function(){
+        $("#cs1").animate({height: "100%"});
+    },
+  });
+
+  $("#cs4").on({
+    mouseenter: function(){
+        $("#cs3").animate({height: "120%"});
+    },
+    mouseleave: function(){
+        $("#cs3").animate({height: "100%"});
+    },
+  });
+
+  $("#cs6").on({
+    mouseenter: function(){
+        $("#cs5").animate({height: "120%"});
+    },
+    mouseleave: function(){
+        $("#cs5").animate({height: "100%"});
+    },
+  });
+
   $("#about").hover(function(){
     $("#cPs").fadeIn();
     $("#uXr").fadeIn("slow");
     $("#vC").fadeIn(3000);
   });
 
-  $("#cs1").hover(function(){
-    $("#txt11").fadeIn();
+
+  $("#fSmol").click(function(){
+    var self = $("p.adjFs").parent();
+    self.toggleClass("biggerFont");
   });
 
-  $("#cs3").hover(function(){
-    $("#txt22").fadeIn();
-  });
-
-  $("#cs4").hover(function(){
-    $("#txt33").fadeIn();
+  $("#fBright").click(function(){
+    var self = $("p.adjFs").parent();
+    self.toggleClass("brighterFont");
   });
